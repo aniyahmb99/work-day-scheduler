1fd724f44882eaab4bbd5e84e983e520df59a677
aniyahmb99 <aniyah_mb@yahoo.com>
2022-03-08 21:13:42 -0500

    worked on current date display and styling

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+var today = moment().format(`dddd, MMM Do YYYY`);
+$("#currentDay").html(today);
+
+// let output = document.querySelector("#currentDay");
+// var today = new Date();
+
+// var dd = today.getDate();
+// var mm = today.getMonth() + 1;
+// var yyyy = today.getFullYear();
+
+// currentDay = `${dd},${mm}, ${yyyy}`;
+// output.innerText = currentDay
+
+
+
+
